c93027f30ad9fda26ccacf6c63c85cafaed6cad3
Kurev <153285455+Kurev@users.noreply.github.com>
2025-10-23 17:25:28 +0800

    Redeploy to Vercel with new env key

--- src/components/project/Location.tsx
+++ src/components/project/Location.tsx
@@ -56,6 +56,7 @@ const Location = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
+
     script.async = true;
     script.defer = true;
     script.onload = () => {
